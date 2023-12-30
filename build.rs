aab0d4ef539b84cb400e9184e5312da475a2eed9
Chris Hennick <hennickc@amazon.com>
2023-12-30 13:37:43 -0800

    Bug fix: static lib location is different on Ubuntu

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 pub fn main() {
-    println!("cargo:rustc-link-search=all=/usr/local/lib");
-    println!("cargo:rustc-link-lib=static=probdist");
-    println!("cargo:rustc-link-lib=static=mylib");
+    println!("cargo:rustc-link-search=all=/usr/lib/x86_64-linux-gnu/");
+    println!("cargo:rustc-link-lib=static=testu01probdist");
+    println!("cargo:rustc-link-lib=static=testu01mylib");
     println!("cargo:rustc-link-lib=static=testu01");
 }
