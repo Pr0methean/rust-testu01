4512b005088ffa12d289c4c42176669b1b21c2d1
Chris Hennick <hennickc@amazon.com>
2023-12-30 13:24:31 -0800

    Bug fix: rustc-link-search before rustc-link-lib, and include dependencies

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 pub fn main() {
+    println!("cargo:rustc-link-search=all=/usr/local/lib");
+    println!("cargo:rustc-link-lib=static=probdist");
+    println!("cargo:rustc-link-lib=static=mylib");
     println!("cargo:rustc-link-lib=static=testu01");
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
 }
