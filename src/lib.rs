42d186340012555bbe6895425e1bedf54691318a
Chris Hennick <hennickc@amazon.com>
2023-12-30 12:52:27 -0800

    Bug fix? Revert crate-type change

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![crate_type = "rlib"]
-
 //! This crate is a wrapper around a small subset of TestU01.
 
 #[macro_use]
