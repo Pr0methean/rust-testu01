817245c2df85ff2c377e3bfbffd2974b94f463c8
Chris Hennick <hennickc@amazon.com>
2023-12-30 13:48:34 -0800

    Support builds on both MacOS and Ubuntu

--- build.rs
+++ build.rs
@@ -1,6 +1,24 @@
-pub fn main() {
-    println!("cargo:rustc-link-search=all=/usr/lib/x86_64-linux-gnu/");
-    println!("cargo:rustc-link-lib=static=testu01probdist");
-    println!("cargo:rustc-link-lib=static=testu01mylib");
-    println!("cargo:rustc-link-lib=static=testu01");
+use std::env;
+use std::env::VarError;
+
+pub fn main() -> Result<(), VarError> {
+    let os = env::var("CARGO_CFG_TARGET_OS")?;
+    match os.as_str() {
+        "macos" => {
+            // WARNING: Tested on a Mac where testu01 was built from source.
+            println!("cargo:rustc-link-search=all=/usr/local/lib/");
+            println!("cargo:rustc-link-lib=static=probdist");
+            println!("cargo:rustc-link-lib=static=mylib");
+            println!("cargo:rustc-link-lib=static=testu01");
+        }
+        "linux" => {
+            // WARNING: Only tested on Ubuntu
+            println!("cargo:rustc-link-search=all=/usr/lib/x86_64-linux-gnu/");
+            println!("cargo:rustc-link-lib=static=testu01probdist");
+            println!("cargo:rustc-link-lib=static=testu01mylib");
+            println!("cargo:rustc-link-lib=static=testu01");
+        }
+        _ => panic!("Don't know how to link testu01 statically on this platform")
+    }
+    Ok(())
 }
