0e646b95d95030f547ab392fdae0f24db69d22bc
Chris Hennick <hennickc@amazon.com>
2023-12-30 16:14:34 -0800

    Update build.rs to handle flattened build from source

--- build.rs
+++ build.rs
@@ -2,7 +2,5 @@ pub fn main() {
     // Tested on MacOS & Ubuntu with testu01 built from source. Dependency names are different when
     // installing it from the Ubuntu repos (testu01probdist and testu01mylib).
     println!("cargo:rustc-link-search=all=/usr/local/lib/");
-    println!("cargo:rustc-link-lib=static=probdist");
-    println!("cargo:rustc-link-lib=static=mylib");
     println!("cargo:rustc-link-lib=static=testu01");
 }
