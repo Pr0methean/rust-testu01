aff376fae752ead8862d6565cb52a8bd5abe0ed6
Chris Hennick <hennickc@amazon.com>
2023-12-30 16:43:36 -0800

    Bug fix: need an explicit path, so we'll save libtestu01.a in /usr/local/lib

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 pub fn main() {
     // Tested on MacOS & Ubuntu with testu01 built from source. Dependency names are different when
     // installing it from the Ubuntu repos (testu01probdist and testu01mylib).
-    println!("cargo:rustc-link-search=all=.");
+    println!("cargo:rustc-link-search=all=/usr/local/lib/");
     println!("cargo:rustc-link-lib=static=testu01");
 }
